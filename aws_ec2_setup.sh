e9b67fe4b38787633021137ed794ee28c973a50d
GMATHUR90 <ecegauravmathur90@gmail.com>
2024-09-18 09:54:48 +0530

    Check git version of instance and updated github workflow yaml file

--- aws_ec2_setup.sh
+++ aws_ec2_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Update, upgrade, and install Vim and Git
-sudo yum update -y && sudo yum upgrade -y && sudo yum install vim -y && sudo yum install git -y
+sudo yum update -y && sudo yum upgrade -y && sudo yum install vim -y && sudo yum install git -y && git --version
