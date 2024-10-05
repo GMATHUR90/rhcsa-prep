338aa5867bad24d7e1cedef363055f23096c65a0
GMATHUR90 <ecegauravmathur90@gmail.com>
2024-10-05 06:50:31 +0530

    Added command line argument with variable and terminal input in scripts

---
+++ shell-scripting/shell_scripts/command_line_arg_terminal_input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " I am learning shell script and my script name is $1 "
+
+date
+
+whoami
+
+echo "$1 is learning about handling variables and command-line arguments in shell script"
+
+
---
+++ shell-scripting/shell_scripts/command_line_arg_with_using_variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+variable_name=Morning_star
+
+echo "I am learning shell script and my script name is $variable_name"
+
+date
+
+whoami
+
+echo "$variable_name is learning about handling variables and command-line arguments"
