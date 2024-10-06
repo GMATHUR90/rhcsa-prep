6b852c593cad6b8c37c4f3782d3fe5e946a6c71b
GMATHUR90 <ecegauravmathur90@gmail.com>
2024-10-06 08:57:22 +0530

    Added a shell script that uses read command in command_line_arg_with_read_command.sh

---
+++ shell-scripting/shell_scripts/command_line_arg_with_read_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter the variable name: " variable_name
+
+echo "I am learning shell script and my script name is $variable_name"
+
+date
+
+whoami
+
+echo "$variable_name is learning about handling variables and command-line arguments"
