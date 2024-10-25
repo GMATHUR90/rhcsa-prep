fa7a97de8917e10ce8f9b9802cf8110c29a29605
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-10-25 06:15:26 +0530

    Update aws_ec2_setup.sh

--- aws_ec2_setup.sh
+++ aws_ec2_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Update, upgrade, and install Vim and Git
+# Update, upgrade, and install Vim and Git in RHEL instances
 sudo yum update -y && sudo yum upgrade -y && sudo yum install vim -y && sudo yum install git -y && git --version
